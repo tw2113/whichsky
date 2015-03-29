fbab3cfee00c1004b85522ece611835a2bba0786
Michael Beckwith <tw2113@gmail.com>
2015-03-29 01:02:13 -0500

    location for future config settings

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace tw2113\WhiskyPalatte;
+use \Slim\Slim as Slim;
+
+return array(
+// MySQL
+    'db.dsn'              => 'mysql:host=localhost;dbname=test',
+    'db.username'         => 'dbuser',
+    'db.password'         => 'dbpass'
+);
