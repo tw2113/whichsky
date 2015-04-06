1b639b807011a32a1455bc56555a2d5a1a6e15d5
Michael Beckwith <tw2113@gmail.com>
2015-04-06 00:42:33 -0500

    remove excessive comment

--- routes/home.php
+++ routes/home.php
@@ -5,7 +5,6 @@ use \Slim\Slim as Slim;
 use \Slim\Logger as Logger;
 use Aura\SqlQuery\QueryFactory;
 
-# Single whisky view.
 $app->get('/', function () use ($app) {
     $app->render('home.php', array(
 		'title' => 'Whichsky Home'
