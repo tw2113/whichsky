ff8ee0bfe606cc3b8b49265a6ffaef6e2368e702
Michael Beckwith <tw2113@gmail.com>
2015-04-07 23:34:28 -0500

    update to plates templating

--- routes/whisky.php
+++ routes/whisky.php
@@ -1,36 +1,16 @@
 <?php
 
 namespace tw2113\Whichsky;
+
 use \Slim\Slim as Slim;
 use \Slim\Logger as Logger;
-use Aura\SqlQuery\QueryFactory;
+use \Aura\SqlQuery\QueryFactory;
+use \League\Plates\Engine as Plates;
 
-# Single whisky view.
-$app->get('/whisky/:id', function ($id) use ($app) {
-	$c['config'] = require dirname( dirname(__FILE__) ) . '/config/config.php';
+$app->get('/whisky/', function() use ($app) {
 
-	$config = $c['config'];
+    $templates = new Plates('./templates');
 
-	$query_factory = new QueryFactory('mysql');
-	$select = $query_factory->newSelect();
-
-	$select->cols( array('id','name','distiller_name') )->from('whiskies')->where( "id = $id");
-
-	// a PDO connection
-	try {
-	    $pdo = new \PDO($config['db.dsn'], $config['db.username'], $config['db.password']);
-
-		$sth = $pdo->prepare($select->__toString());
-		// bind the values and execute
-		$sth->execute($select->getBindValues());
-		// get the results back as an associative array
-		$result = $sth->fetch(\PDO::FETCH_ASSOC);
-	} catch (\Exception $e) {
-	    echo 'Caught exception: ',  $e->getMessage(), "\n";
-	}
-
-    $app->render('whisky.php', array(
-		'title' => $result['name'],
-		'body'  => ['id' => $result['id'], 'name' => $result['name'], 'distillery' => $result['distiller_name'] ]
-	));
+    // Render a template
+    echo $templates->render('whisky');
 });
