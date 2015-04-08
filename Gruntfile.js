7bb3c587a1f107aa4d446ab3f9bc2317a196e33c
Michael Beckwith <tw2113@gmail.com>
2015-04-07 23:17:51 -0500

    forgot to remove unneeded tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,11 +51,9 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-sass');
 
   grunt.registerTask('default', ['concat', 'uglify', 'watch']);
