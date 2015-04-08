4aae7315afe807e6dab8cde51e589e8899464f3a
Michael Beckwith <tw2113@gmail.com>
2015-04-07 23:34:43 -0500

    add plates templating

--- routes/wishlist.php
+++ routes/wishlist.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace tw2113\Whichsky;
+
+use \Slim\Slim as Slim;
+use \Slim\Logger as Logger;
+use \Aura\SqlQuery\QueryFactory;
+use \League\Plates\Engine as Plates;
+
+$app->get('/wishlist/', function() use ($app) {
+
+    $templates = new Plates('./templates');
+
+    // Render a template
+    echo $templates->render('wishlist');
+});
