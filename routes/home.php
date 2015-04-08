c28bf10f1b660cc935b94d5cff134a1cd3308eb1
Michael Beckwith <tw2113@gmail.com>
2015-04-07 23:33:53 -0500

    switch home route file to plates

--- routes/home.php
+++ routes/home.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace tw2113\Whichsky;
+
 use \Slim\Slim as Slim;
 use \Slim\Logger as Logger;
-use Aura\SqlQuery\QueryFactory;
+use \Aura\SqlQuery\QueryFactory;
+use \League\Plates\Engine as Plates;
 
-$app->get('/', function () use ($app) {
-    $app->render('home.php', array(
-		'title' => 'Whichsky Home'
-	));
+$app->get('/', function() use ($app) {
+
+    $templates = new Plates('./templates');
+
+    // Render a template
+    echo $templates->render('home', ['name' => 'Michael']);
 });
