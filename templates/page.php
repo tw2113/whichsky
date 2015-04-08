5fe6c066308aef4f5ffb60b6b3558913dd4c3611
Michael Beckwith <tw2113@gmail.com>
2015-04-07 23:19:24 -0500

    add platesphp-based template file

---
+++ templates/page.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title><?php echo $title; ?></title>
+<meta name="viewport" content="width=device-width">
+
+<script src="/templates/assets/js/whichsky.js"></script>
+<link rel="stylesheet" href="/templates/assets/css/style.css">
+<body>
+
+<?php echo $this->section('content'); ?>
+
+</body>
+</html>
