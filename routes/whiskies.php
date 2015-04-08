b96fc19681742e282cd24dd1c3952f5c9ff3cda7
Michael Beckwith <tw2113@gmail.com>
2015-04-07 23:34:16 -0500

    update to plates templating

--- routes/whiskies.php
+++ routes/whiskies.php
@@ -1,31 +1,16 @@
 <?php
 
-# Single whisky view.
-$app->get('/whiskies/', function ($id) use ($app) {
-	$c['config'] = require dirname(__FILE__) . '../config/config.php';
+namespace tw2113\Whichsky;
 
-	$config = $c['config'];
+use \Slim\Slim as Slim;
+use \Slim\Logger as Logger;
+use \Aura\SqlQuery\QueryFactory;
+use \League\Plates\Engine as Plates;
 
-	$query_factory = new QueryFactory('mysql');
-	$select = $query_factory->newSelect();
+$app->get('/whiskies/', function() use ($app) {
 
-	$select->cols( array('id','name','distiller_name') )->from('whiskies')->where( "id = $id");
+    $templates = new Plates('./templates');
 
-	// a PDO connection
-	try {
-	    $pdo = new \PDO($config['db.dsn'], $config['db.username'], $config['db.password']);
-
-		$sth = $pdo->prepare($select->__toString());
-		// bind the values and execute
-		$sth->execute($select->getBindValues());
-		// get the results back as an associative array
-		$result = $sth->fetch(\PDO::FETCH_ASSOC);
-	} catch (\Exception $e) {
-	    echo 'Caught exception: ',  $e->getMessage(), "\n";
-	}
-
-    $app->render('home.php', array(
-		'title' => 'Hello Slim.',
-		'body'  => ['id' => $result['id'], 'name' => $result['name'], 'distillery' => $result['distiller_name'] ]
-	));
+    // Render a template
+    echo $templates->render('whiskies');
 });
