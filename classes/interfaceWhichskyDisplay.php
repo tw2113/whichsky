b49bbb755721993d1f9c24156210d9498c8b7271
Michael Beckwith <tw2113@gmail.com>
2015-04-12 20:23:22 -0500

    add our display interface file

---
+++ classes/interfaceWhichskyDisplay.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tw2113\Whichsky;
+
+/**
+ * Interface Display
+ * @package tw2113\Whichsky
+ */
+interface Display {
+
+    public function listing( $list );
+
+    public function item( $item );
+}
