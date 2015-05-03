808e4086ff468871a0a658a434941e2007889d44
Michael Beckwith <tw2113@gmail.com>
2015-05-02 20:16:38 -0500

    begin constructing form and send results to template

--- routes/manage.php
+++ routes/manage.php
@@ -12,6 +12,54 @@ $app->get('/manage/', function() use ($app) {
 
     $templates = new Plates('./templates');
 
+    $builder = new FormBuilder;
+
+    $fields = array(
+        array( 'type' => 'text', 'label' => 'Whisky Name', 'id' => 'whisky_name' ),
+        array( 'type' => 'text', 'label' => 'Distilllery Name', 'id' => 'distillery_name' ),
+        array( 'type' => 'text', 'label' => 'Years Matured', 'id' => 'years_matured' ),
+        array( 'type' => 'text', 'label' => 'Style', 'id' => 'style' ),
+        array( 'type' => 'text', 'label' => 'Years Matured', 'id' => 'years_matured' ),
+        array( 'type' => 'text', 'label' => 'Volume', 'id' => 'volume' ),
+        array( 'type' => 'text', 'label' => 'Price', 'id' => 'price' ),
+        array( 'type' => 'text', 'label' => 'Date Purchased', 'id' => 'date_purchased' ),
+        array( 'type' => 'text', 'label' => 'Date Opened', 'id' => 'date_opened' ),
+        array( 'type' => 'text', 'label' => 'ABV', 'id' => 'abv' ),
+        array( 'type' => 'textarea', 'label' => 'Packaging Description', 'id' => 'packaging_description' ),
+        array( 'type' => 'textarea', 'label' => 'Aroma', 'id' => 'aroma' ),
+        array( 'type' => 'textarea', 'label' => 'Palet', 'id' => 'palet' ),
+        array( 'type' => 'textarea', 'label' => 'Finish', 'id' => 'finish' ),
+    );
+
+    $form  = $builder->open()->addClass('pure-form');
+
+    foreach ( $fields as $field ) {
+        $form .= '<div class="pure-u-1-1">';
+        switch ( $field['type'] ) {
+            case 'text':
+                $form .= $builder->label( $field['label'] )->forId( $field['id'] ) . '<br/>';
+                $form .= $builder->text( $field['id'] )->id( $field['id'] ) . '<br/>';
+                break;
+
+            case 'textarea':
+                $form .= $builder->label( $field['label'] )->forId( $field['id'] ) . '<br/>';
+                $form .= $builder->textarea( $field['id'] )->id( $field['id'] )->rows(5);
+                break;
+        }
+        $form .= '</div>';
+    }
+
+    $form .= $builder->button('Add whisky')->addClass('pure-button button-secondary button-xlarge');
+    $form .= $builder->close();
+
+    /*
+picture
+on_wishlist
+comments
+     */
+
+
+    $data['form'] = $form;
     // Render a template
     echo $templates->render('manage');
 });
