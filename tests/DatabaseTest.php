a4caa4848f37ce43d0bfb51e1e69e1e6e8872b03
Michael Beckwith <tw2113@gmail.com>
2015-07-27 01:58:14 -0500

    set up Database test class a bit more

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -1,8 +1,12 @@
 <?php
 
-use tw2113\Whichsky\Database;
+namespace tw2113\Whichsky;
 
-class DatabaseTest extends PHPUnit_Framework_TestCase
+require __DIR__ . '/../vendor/autoload.php';
+
+class DatabaseTest extends \PHPUnit_Framework_TestCase
 {
+    public function test_Database_Connection() {
 
+    }
 }
