68c45dbaef1cff1d7493aac080c3f111f6f7bd47
Michael Beckwith <tw2113@gmail.com>
2015-07-27 02:03:23 -0500

    remove autoload.php reference since bootstrap covers us now

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -2,8 +2,6 @@
 
 namespace tw2113\Whichsky;
 
-require __DIR__ . '/../vendor/autoload.php';
-
 class DatabaseTest extends \PHPUnit_Framework_TestCase
 {
     public function test_Database_Connection() {
