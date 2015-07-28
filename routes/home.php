237212dba7cec3155bb14104c0b74671e3964fb3
Michael Beckwith <tw2113@gmail.com>
2015-07-28 01:33:52 -0500

    remove some likely unused namespaces

--- routes/home.php
+++ routes/home.php
@@ -2,10 +2,8 @@
 
 namespace tw2113\Whichsky;
 
-use \Slim\Slim as Slim;
 use \Slim\Logger as Logger;
 use \Aura\SqlQuery\QueryFactory;
-use \League\Plates\Engine as Plates;
 
 $app->get('/', function() use ($app) {
 
