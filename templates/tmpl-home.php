9ba0f0960a78046dc8192b0ffbd1d8edf5de596f
Michael Beckwith <tw2113@gmail.com>
2015-12-07 23:41:10 -0600

    move to inline php as per Plates recommendation

--- templates/tmpl-home.php
+++ templates/tmpl-home.php
@@ -1,6 +1,6 @@
 <?php $this->layout( 'tmpl-page', [ 'title' => 'Whichsky' ] ) ?>
 
 <div class="pure-u-1-1">
-    <h1><?php echo "Welcome to Whichsky, {$this->e($name)}! Your own personal Whisky tasting and wishlist application."; ?></h1>
+    <h1>Welcome to Whichsky, <?= $this->e($name) ?>! Your own personal Whisky tasting and wishlist application.</h1>
 </div>
 <div class="pure-u-1-2"></div>
