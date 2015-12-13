21a428d4946b891df49daac0e897f07567b8f1f7
Michael Beckwith <tw2113@gmail.com>
2015-12-13 14:05:20 -0600

    more work on the install route and table creation

--- routes/install.php
+++ routes/install.php
@@ -2,14 +2,62 @@
 
 namespace tw2113\Whichsky;
 
-use \Aura\SqlQuery\QueryFactory;
+use \Slim\Slim as Slim;
+use Aura\Sql\ExtendedPdo;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$app->get( '/install', function () use ( $app ) {
+$app->get( '/install', function ( $request, $response, $args ) use ( $app ) {
+
+    $config = $app->config;
+
+    $whiskies_table_create = "CREATE TABLE IF NOT EXISTS whiskies (
+        whisky_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        whisky_name TEXT,
+        distiller_name TEXT,
+        alcohol_by_volume TEXT,
+        package_description TEXT,
+        years_matured TEXT,
+        whisky_style TEXT,
+        whisky_volume TEXT,
+        purchase_price TEXT,
+        aroma_description TEXT,
+        palate_description TEXT,
+        finish_description TEXT
+    )";
+
+    $distilleries_table_create = "CREATE TABLE IF NOT EXISTS distilleries (
+        distillery_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        distillery_name TEXT,
+        distillery_location TEXT,
+        year_established TEXT,
+        website_url TEXT
+    )";
+
+    $pdo = new ExtendedPdo(
+        $config['db.pdo.connect']
+    );
+
+    try {
+        $pdo->exec( $whiskies_table_create );
+    } catch ( \Exception $e ) {
+        $log = new Logger( 'install_errors' );
+        $log->pushHandler( new StreamHandler( 'logs/error.log', Logger::WARNING ) );
+
+        $log->addError( 'Whiskies table exception: ' . $e->getMessage() );
+    }
+
+    try {
+        $pdo->exec( $distilleries_table_create );
+    } catch ( \Exception $e ) {
+        $log = new Logger( 'install_errors' );
+        $log->pushHandler( new StreamHandler( 'logs/error.log', Logger::WARNING ) );
+
+        $log->addError( 'Distilleries table exception: ' . $e->getMessage() );
+    }
 
     $templates = $app->plates;
 
     // Render a template
-    echo $templates->render( 'tmpl-home', [ 'name' => 'Michael' ] );
+    echo $templates->render( 'tmpl-install', [ 'name' => 'Michael' ] );
 } );
---
+++ templates/tmpl-install.php
@@ -0,0 +1,7 @@
+<?php $this->layout( 'tmpl-page', [ 'title' => 'Whichsky' ] ) ?>
+
+<div class="pure-u-1-1">
+    <h1>Thank you for installing Whichsky!</h1>
+    <p><?= $this->e($name) ?></p>
+</div>
+<div class="pure-u-1-2"></div>
