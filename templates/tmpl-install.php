f62630842b2d8e80b11deb49b2289706e102f310
Michael Beckwith <tw2113@gmail.com>
2015-12-14 00:14:22 -0600

    output a proper install thank you message

--- templates/tmpl-install.php
+++ templates/tmpl-install.php
@@ -2,6 +2,7 @@
 
 <div class="pure-u-1-1">
     <h1>Thank you for installing Whichsky!</h1>
-    <p><?= $this->e($name) ?></p>
 </div>
-<div class="pure-u-1-2"></div>
+<div class="pure-u-1-1">
+    <p>You should now be able to begin managing your whisky history and wishlist. Enjoy!</p>
+</div>
