a43f4fc0b84f5ac0b15f67179f3373376cd0413a
Michael Beckwith <tw2113@gmail.com>
2015-12-14 00:38:31 -0600

    add error message to output

--- templates/tmpl-manage.php
+++ templates/tmpl-manage.php
@@ -3,5 +3,9 @@
 <div class="pure-u-1-1">
     <h1>This page will be where you manage inventory.</h1>
 
+    <?php if ( isset( $error ) ) : ?>
+    <p>There was an error processing the whisky.</p>
+    <?php endif; ?>
+
     <?= $form ?>
 </div>
